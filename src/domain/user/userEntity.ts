1370b0f2515eec90eebcfa5e837f6ba5c4daf7f6
dodoyoo <dong15903@daum.net>
2025-08-31 17:04:01 +0900

    feat: userEntity

--- src/domain/user/userEntity.ts
+++ src/domain/user/userEntity.ts
@@ -5,6 +5,7 @@ import {
   PrimaryGeneratedColumn,
   Timestamp,
   UpdateDateColumn,
+  CreateDateColumn,
 } from 'typeorm';
 
 @Entity('users')
@@ -14,4 +15,29 @@ export class User {
 
   @Column({ type: 'varchar', length: 255, nullable: false, unique: true })
   email!: string;
+
+  @Column({ type: 'varchar', length: 100, nullable: false })
+  password!: string;
+
+  @Column({ type: 'varchar', length: 50, nullable: false })
+  nickname!: string;
+
+  @Column({ type: 'varchar', length: 2048, nullable: true })
+  profile_image!: string;
+
+  @Column({ type: 'tinyint', width: 1, nullable: false, default: 0 })
+  is_verified: boolean = false;
+
+  @Column({ type: 'varchar', length: 500, nullable: false })
+  email_verification_token!: string;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at!: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    nullable: false,
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at!: Date;
 }
