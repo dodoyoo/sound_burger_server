ac4f1bb7b5157bc40fcb2a64edceff315fe263f8
dodoyoo <dong15903@daum.net>
2025-09-01 17:33:48 +0900

    유저 레포 작성중

--- src/domain/user/userRepository.ts
+++ src/domain/user/userRepository.ts
@@ -1,3 +1,11 @@
 import { Repository } from 'typeorm';
 import { AppDataSource } from '../../models/dataSource';
 import { User } from './userEntity';
+
+export class UserRepository {
+  private repository: Repository<User>;
+
+  constructor(dataSource?: typeof AppDataSource) {
+    this.repository = (dataSource || AppDataSource).getRepository(User);
+  }
+}
