7bf952b154f4e4ad0eeda88c82064a7f3f4a7e9b
dodoyoo <dong15903@daum.net>
2025-09-02 18:25:28 +0900

    signUp repository

--- src/domain/user/userRepository.ts
+++ src/domain/user/userRepository.ts
@@ -8,4 +8,15 @@ export class UserRepository {
   constructor(dataSource?: typeof AppDataSource) {
     this.repository = (dataSource || AppDataSource).getRepository(User);
   }
+
+  // 유저 회원가입
+  async createUser(userData: Partial<User>): Promise<User> {
+    try {
+      const user = this.repository.create(userData);
+      return await this.repository.save(user);
+    } catch (error) {
+      console.error('Error creating user:', error);
+      throw new Error('Could not create user');
+    }
+  }
 }
