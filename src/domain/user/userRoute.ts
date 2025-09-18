12a5501bd8cc9af0fcab4cc094849129d83ab423
dodoyoo <dong15903@daum.net>
2025-09-18 16:26:14 +0900

    top100 노래 불러오기 제작중

--- src/domain/user/userRoute.ts
+++ src/domain/user/userRoute.ts
@@ -11,4 +11,9 @@ router.post('/api/sign-up', (req, res) => userController.signUp(req, res));
 router.post('/api/sign-in', (req, res) => userController.signIn(req, res));
 
 router.get('/api/verify', (req, res) => userController.verifyEmail(req, res));
+
+router.get('/api/spotify/top100', (req, res) =>
+  userController.getTop100(req, res)
+);
+
 export default router;
